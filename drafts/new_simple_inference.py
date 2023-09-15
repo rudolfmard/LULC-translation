0e1da3d70ffced0a5b94153ce03c713a12b2e97f
Thomas Rieutord <thomas.rieutord@gmail.com>
2023-09-15 16:07:54 +0000

    Re-write the simple inference program using the I/O module

---
+++ drafts/new_simple_inference.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Multiple land-cover/land-use Maps Translation (MMT)
+
+Program to make prediction with MLCT-net.
+"""
+import os
+import sys
+import torch
+import matplotlib.pyplot as plt
+import rasterio.crs
+from mmt.inference import io
+from mmt.datasets import transforms as mmt_transforms
+import wopt.ml.utils
+from wopt.ml import (
+    landcovers,
+    transforms,
+    domains,
+)
+
+# Configs
+#---------
+xp_name = "aaunet2"
+domainname = "dublin_city"
+lc_in="esawc"
+lc_out="esgp"
+usegpu = True
+device = torch.device("cuda" if usegpu else "cpu")
+
+woptconfig = wopt.ml.utils.load_config()
+print(f"Executing program {sys.argv[0]} from {os.getcwd()}")
+
+
+# Landcovers
+#------------
+dst_crs = rasterio.crs.CRS.from_epsg(3035)
+
+print(f"Loading landcovers with CRS = {dst_crs}")
+in_lc = landcovers.ESAWorldCover(
+    transforms=transforms.fdiv_10_minus_1,
+    crs = dst_crs,
+)
+in_lc.crs = dst_crs
+in_lc.res = 10
+
+out_lc = landcovers.EcoclimapSGplus(crs=dst_crs, res=60)
+out_lc.crs=dst_crs
+out_lc.res=60
+
+
+# Loading query
+#----------------
+qdomain = getattr(domains, domainname)
+qb = qdomain.to_tgbox(dst_crs)
+
+t1h = mmt_transforms.OneHot(in_lc.n_labels + 1, device = device)
+x = t1h(in_lc[qb]["mask"])
+k = out_lc.res/in_lc.res
+ccrop = transforms.t.CenterCrop(size=[int(k*(d // k)) for d in x.shape[-2:]])
+x = ccrop(x)
+
+# Loading model
+#----------------
+print(f"Loading auto-encoders from {xp_name}")
+model = io.load_pytorch_model(xp_name, lc_in, lc_out)
+model = model.to(device)
+
+# Apply model
+#----------------
+with torch.no_grad():
+    logits = model(x)
+
+y = logits.detach().argmax(1).cpu()
+
+print(f"Show inference on {domainname}")
+out_lc.plot({"mask":y})
+plt.show(block=False)
