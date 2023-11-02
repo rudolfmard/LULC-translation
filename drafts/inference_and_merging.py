b346b38fdb38e6fa5b07aa25382a917c1b364b29
Thomas Rieutord <thomas.rieutord@gmail.com>
2023-11-02 17:33:26 +0000

    Fix issue with sampler of length 0 for large values of n_px_max in merger

--- drafts/inference_and_merging.py
+++ drafts/inference_and_merging.py
@@ -18,7 +18,8 @@ from mmt.inference import translators
 checkpoint_path=os.path.join(mmt_repopath, "saved_models", "vanilla_eurat3.ep169.ckpt")
 # classifier_path=os.path.join(mmt_repopath, "saved_models", "rfc_1000trees.pkl")
 domainname = "eurat"
-inference_dump_dir = os.path.join(mmt_repopath, "data", "output")
+n_px_max = 5400 # Maximum that could fit on the GPU
+inference_dump_dir = os.path.join(mmt_repopath, "data", "outputs")
 
 # Load translators
 #------------
@@ -31,7 +32,7 @@ qdomain = getattr(domains, domainname)
 inference_tif_dir = translator.predict_from_large_domain(
     qdomain,
     output_dir=os.path.join(inference_dump_dir, f"{domainname}.[id]"),
-    tmp_dir=f"{domainname}.[id]",
+    tmp_dir=os.path.join(inference_dump_dir, "eurat.tmp.2e7cat.01Nov-17h29"),#f"{domainname}.[id]"),
     n_px_max=5400,
     n_max_files = 0,
 )
@@ -40,11 +41,13 @@ inference_tif_dir = translator.predict_from_large_domain(
 #------------
 print("Merging the inference with ECOSG+")
 merger = translators.MapMerger(inference_tif_dir)
+print(f"Merging criterion: {merger.merge_criterion.__doc__}")
 merging_dump_dir = merger.predict_from_large_domain(
     qdomain,
     output_dir=os.path.join(inference_dump_dir, "ECOSGML-v0.4-[id]"),
-    tmp_dir=f"merger.{domainname}.[id]",
-    n_px_max = 900,
+    tmp_dir=os.path.join(inference_dump_dir, f"merger.{domainname}.[id]"),
+    n_px_max = 828, # Make sure that the patches are smaller than the size of a file
+    # Thumb rule: < n_px_max*esawc.res/esgp.res
     n_max_files = 200,
 )
 print(f"Merged map created at {merging_dump_dir}")
