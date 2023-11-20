917038cd743ec46da88c1363a32e7c80f2adf0b4
Thomas Rieutord <thomas.rieutord@gmail.com>
2023-11-20 15:38:38 +0000

    New classes for ensemble land cover: ProbaLandCover and InferenceResultsProba

--- mmt/datasets/landcovers.py
+++ mmt/datasets/landcovers.py
@@ -6,6 +6,7 @@ Land cover maps
 """
 import os
 import time
+import torch
 import numpy as np
 import netCDF4 as nc
 import rasterio
@@ -19,6 +20,9 @@ from typing import Any, Dict, Optional
 
 from mmt import _repopath_ as mmt_repopath
 from mmt.utils import config as utilconf
+
+# VARIABLES
+# ============
 
 config = utilconf.get_config_from_yaml(
     os.path.join(mmt_repopath, "configs", "new_config_template.yaml")
@@ -128,6 +132,9 @@ def parse_version_infos(path):
             infos[k] = v.split('"')[1]
         
     return infos
+
+# BASE CLASSES
+# ============
 
 class TorchgeoLandcover(tgd.RasterDataset):
     """Abstract class for land cover dataset using TorchGeo.
@@ -241,6 +248,265 @@ class TorchgeoLandcover(tgd.RasterDataset):
         # fig.tight_layout()
         return fig, ax
 
+
+class ProbaLandcover(tgd.RasterDataset):
+    """Abstract class for land cover probability dataset using TorchGeo.
+    
+    This class is a [customised TorchGeo RasterDataset](https://torchgeo.readthedocs.io/en/latest/tutorials/custom_raster_dataset.html).
+    The customisation is in the precision of the path where to find the data and several
+    attributes that are common to all land covers probability classes.
+    
+    
+    Parameters
+    ----------
+    transforms: Optional[Callable[[dict[str, Any]], dict[str, Any]]]
+        A function/transform that takes an input sample and returns a transformed version
+    
+    
+    Main attributes
+    ---------------
+    path: str
+        Absolute path to the root directory containing the data
+        
+    filename_glob: str
+        Glob expression used to search for files
+
+    is_image: bool=True
+        Probabilities are seen as imagery to avoid casting data to integers
+
+    separate_files: bool=False
+        True if data is stored in a separate file for each band, else False.
+    
+    crs: `rasterio.crs.CRS`
+        Coordinate reference system in which the land cover is
+    
+    labels: list of str
+        Labels names
+    
+    cmap: list of 3-tuple
+        Colormap for each 
+    
+    
+    Main methods
+    ------------
+    __getitem__: Return a sample of data
+    plot: Plot a sample of data
+    """
+    path = ""
+    filename_glob = "*.tif"
+    is_image = True
+    separate_files = False
+    crs = None
+    labels = []
+    cmap = []
+    
+    def __init__(self, crs = None, res = None, transforms = None, tgeo_init = True):
+        self.n_labels = len(self.labels)
+        if tgeo_init:
+            super().__init__(self.path, crs = crs, res = res, transforms = transforms)
+        
+    def plot(
+        self,
+        sample: Dict[str, Any],
+        show_titles: bool = True,
+        title: Optional[str] = None,
+        suptitle: Optional[str] = None,
+        figax = None
+    ):
+        """Plot a sample of data.
+
+
+        Parameters
+        ----------
+        sample: dict
+            Sample of data. Labels must be accessible under the 'mask' key.
+
+        show_titles: bool, default=True
+            True if a title is diplayed over the figure.
+
+        suptitle: str
+            If provided, the string given here is put as main title of the figure.
+        """
+        assert len(self.labels) == len(self.cmap), f"The number of labels ({len(self.labels)}) do not match the number of colors ({len(self.cmap)})"
+        
+        cmap = np.array(self.cmap)/255
+        if figax is None:
+            fig, ax = plt.subplots(1, 1, figsize=(12,12))
+        else:
+            fig, ax = figax
+        
+        proba = sample["image"].squeeze().permute(1,2,0)
+        
+        x = np.matmul(proba[:,:,:-1], cmap)
+        im = ax.imshow(x)
+        if show_titles:
+            if title is None:
+                title = self.__class__.__name__
+            
+            ax.set_title(title)
+            
+        if suptitle is not None:
+            plt.suptitle(suptitle)
+        
+        return fig, ax
+        
+    def plot_uncertainty(
+        self,
+        sample: Dict[str, Any],
+        show_titles: bool = True,
+        show_colorbar: bool = True,
+        title: Optional[str] = None,
+        suptitle: Optional[str] = None,
+        logscale: bool = False,
+        figax = None
+    ):
+        """Plot an estimate of the uncertainty on the land cover label from the land cover probabilities
+
+
+        Parameters
+        ----------
+        sample: dict
+            Sample of data. Labels must be accessible under the 'mask' key.
+
+        show_titles: bool, default=True
+            True if a title is diplayed over the figure.
+
+        suptitle: str
+            If provided, the string given here is put as main title of the figure.
+        """
+        
+        if figax is None:
+            fig, ax = plt.subplots(1, 1, figsize=(12,12))
+        else:
+            fig, ax = figax
+        
+        proba = sample["image"].squeeze().softmax(0)
+        
+        psort, _ = proba.sort(0)
+        uq = psort[-2]/psort[-1]
+        
+        if logscale:
+            im = ax.imshow(torch.log(uq), cmap = "brg")
+        else:
+            im = ax.imshow(uq, cmap = "brg", vmin = 0, vmax = 1)
+        
+        if show_titles:
+            if title is None:
+                title = "Uncertainty " + self.__class__.__name__
+            
+            ax.set_title(title)
+            
+        if show_colorbar:
+            cbar = plt.colorbar(im)
+        
+        if suptitle is not None:
+            plt.suptitle(suptitle)
+        
+        return fig, ax
+    
+    def generate_member(self, sample, u = None, print_u = False):
+        """Generate a land cover member for the given probability sample"""
+        if u is None:
+            u = torch.rand(1)
+            if print_u:
+                print("u=", u)
+        
+        proba = sample["image"].squeeze()
+        cdf = proba.cumsum(0)/proba.sum(0)
+        labels = (cdf < u).sum(0)
+        
+        return labels
+
+
+class OpenStreetMap:
+    """OpenStreetMap land cover from Cartopy (for plot only).
+    
+    The class is init with a level of details and a patch size. It is then
+    used to produce Cartopy plots of OSM land cover at given coordinates +/- patch size.
+    Coordinates and patch size are expected to be in lon/lon format (EPSG:4326).
+    
+    
+    Parameters
+    ----------
+    details: int, default=3
+        Level of details in the map. The higher, the more detailled but the heavier to load
+        
+    default_patch_size: float, default=0.05
+        Patch size in lon/lat degrees. Overwritten by plot argument, if provided.
+    """
+    def __init__(self, details = 3, default_patch_size = 0.05):
+        self.details = details
+        self.default_patch_size = default_patch_size
+        self.background_image = cimgt.OSM()
+    
+    def plot(
+        self,
+        sample: Dict[str, Any],
+        patch_size: Optional[float] = None,
+        show_titles: bool = True,
+        figax = None,
+        rowcolidx = 111
+    ):
+        """Plot the OpenStreetMap land cover
+        
+        
+        Parameters
+        ----------
+        sample: dict
+            Sample with a 'coordinate' or 'bbox' key that will be used to
+            specify the location.
+            The 'bbox' must have [minx, ..., maxy] attributes
+            The 'coordinate' is assumed to point to the upper-left corner and will be completed by `patch_size`
+            All location information are expected to be in lon/lat degrees
+        
+        patch_size: int, optional
+            Patch size to use when the sample only has a 'coordinate' key.
+            If not provided, the default patch size set in init is used.
+        
+        
+        """
+        
+        if figax is None:
+            fig, ax = plt.subplots(1, 1, figsize=(12,12))
+        else:
+            fig, ax = figax
+            ax.set_axis_off()
+        
+        if patch_size is None:
+            patch_size = self.default_patch_size
+        
+        if "bbox" in sample.keys():
+            minx = sample["bbox"].minx
+            miny = sample["bbox"].miny
+            maxx = sample["bbox"].maxx
+            maxy = sample["bbox"].maxy
+        elif "coordinate" in sample.keys():
+            # Assume they correspond to the upper-left corner (image convention)
+            minx, maxy = sample["coordinate"]
+            maxx = minx + patch_size
+            miny = maxy - patch_size
+        else:
+            raise ValueError("Sample does not have geographical info")
+        
+        locextent = [minx, maxx, miny, maxy]
+        xticks = np.linspace(locextent[0], locextent[1], 5)
+        yticks = np.linspace(locextent[2], locextent[3], 5)
+        
+        ax0 = fig.add_subplot(rowcolidx, projection=self.background_image.crs)
+        ax0.set_extent(locextent)
+        ax0.add_image(self.background_image, self.details)
+        ax0.set_xticks(xticks, crs = ccrs.PlateCarree())
+        ax0.set_yticks(yticks, crs = ccrs.PlateCarree())
+        ax0.set_xticklabels(np.round(xticks,3))
+        ax0.set_yticklabels(np.round(yticks,3))
+        if show_titles:
+            ax0.set_title(self.__class__.__name__)
+        
+        return fig, ax0
+    
+
+# CHILD CLASSES
+# =============
 
 class EcoclimapSG(TorchgeoLandcover):
     path = os.path.join(config.paths.data_dir, "tiff_data", "ECOCLIMAP-SG")
@@ -357,6 +623,7 @@ class EcoclimapSGplus(TorchgeoLandcover):
         
         return ofn_nc
 
+
 class QualityFlagsECOSGplus(EcoclimapSGplus):
     path = os.path.join(config.paths.data_dir, "tiff_data", "ECOCLIMAP-SG-plus", f"ecosgp-qflags-v{config.versions.ecosgplus}")
     labels = [
@@ -372,98 +639,31 @@ class QualityFlagsECOSGplus(EcoclimapSGplus):
     
 
 class InferenceResults(EcoclimapSGplus):
+    """ECOSG-like land cover maps (same labels) loaded from a given path"""
+    def __init__(self, path, crs = None, res = None, transforms = None, tgeo_init = True):
+        self.path = path
+        self.n_labels = len(self.labels)
+        super().__init__(crs = crs, res = res, transforms = transforms, tgeo_init = tgeo_init)
+
+
+class InferenceResultsProba(ProbaLandcover):
+    """ECOSG-like land cover probability maps (same labels) loaded from a given path"""
+    labels = ecoclimapsg_labels
+    cmap = [
+        (0,0,0),(0,0,128),(0,0,205),(0, 0, 255),(211,211,211),(169,169,169),(255,250,250),
+        (240,255,240),(85,107,47),(154,205,50),(0,128,0),(255,127,80),(160,82,45),(34,139,34),
+        (188,143,143),(205,133,63),(222,184,135),(50,205,50),(255,215,0),(32,178,170),(173,255,47),
+        (189,183,107),(102,102,0),(46,139,87),(138,2,0),(206,0,0),(252,1,1),(255,90,0),(255,120,0),
+        (255,150,0),(255,180,0),(255,210,0),(255,240,0),(128,128,128)
+    ]
+    crs = rasterio.crs.CRS.from_epsg(4326)
     
     def __init__(self, path, crs = None, res = None, transforms = None, tgeo_init = True):
         self.path = path
         self.n_labels = len(self.labels)
         super().__init__(crs = crs, res = res, transforms = transforms, tgeo_init = tgeo_init)
 
-class EcoclimapSGML(EcoclimapSGplus):
-    path = os.path.join(config.paths.data_dir, "tiff_data", "ECOCLIMAP-SG-ML", "tif", f"ECOCLIMAP-SG-ML-v{config.versions.ecosgml}")
 
-class OpenStreetMap:
-    """OpenStreetMap land cover from Cartopy (for plot only).
-    
-    The class is init with a level of details and a patch size. It is then
-    used to produce Cartopy plots of OSM land cover at given coordinates +/- patch size.
-    Coordinates and patch size are expected to be in lon/lon format (EPSG:4326).
-    
-    
-    Parameters
-    ----------
-    details: int, default=3
-        Level of details in the map. The higher, the more detailled but the heavier to load
-        
-    default_patch_size: float, default=0.05
-        Patch size in lon/lat degrees. Overwritten by plot argument, if provided.
-    """
-    def __init__(self, details = 3, default_patch_size = 0.05):
-        self.details = details
-        self.default_patch_size = default_patch_size
-        self.background_image = cimgt.OSM()
-    
-    def plot(
-        self,
-        sample: Dict[str, Any],
-        patch_size: Optional[float] = None,
-        show_titles: bool = True,
-        figax = None,
-        rowcolidx = 111
-    ):
-        """Plot the OpenStreetMap land cover
-        
-        
-        Parameters
-        ----------
-        sample: dict
-            Sample with a 'coordinate' or 'bbox' key that will be used to
-            specify the location.
-            The 'bbox' must have [minx, ..., maxy] attributes
-            The 'coordinate' is assumed to point to the upper-left corner and will be completed by `patch_size`
-            All location information are expected to be in lon/lat degrees
-        
-        patch_size: int, optional
-            Patch size to use when the sample only has a 'coordinate' key.
-            If not provided, the default patch size set in init is used.
-        
-        
-        """
-        
-        if figax is None:
-            fig, ax = plt.subplots(1, 1, figsize=(12,12))
-        else:
-            fig, ax = figax
-            ax.set_axis_off()
-        
-        if patch_size is None:
-            patch_size = self.default_patch_size
-        
-        if "bbox" in sample.keys():
-            minx = sample["bbox"].minx
-            miny = sample["bbox"].miny
-            maxx = sample["bbox"].maxx
-            maxy = sample["bbox"].maxy
-        elif "coordinate" in sample.keys():
-            # Assume they correspond to the upper-left corner (image convention)
-            minx, maxy = sample["coordinate"]
-            maxx = minx + patch_size
-            miny = maxy - patch_size
-        else:
-            raise ValueError("Sample does not have geographical info")
-        
-        locextent = [minx, maxx, miny, maxy]
-        xticks = np.linspace(locextent[0], locextent[1], 5)
-        yticks = np.linspace(locextent[2], locextent[3], 5)
-        
-        ax0 = fig.add_subplot(rowcolidx, projection=self.background_image.crs)
-        ax0.set_extent(locextent)
-        ax0.add_image(self.background_image, self.details)
-        ax0.set_xticks(xticks, crs = ccrs.PlateCarree())
-        ax0.set_yticks(yticks, crs = ccrs.PlateCarree())
-        ax0.set_xticklabels(np.round(xticks,3))
-        ax0.set_yticklabels(np.round(yticks,3))
-        if show_titles:
-            ax0.set_title(self.__class__.__name__)
-        
-        return fig, ax0
-    
+class EcoclimapSGML(EcoclimapSGplus):
+    """ECOCLIMAP-SG-ML land cover map. Release merge between ECOSG+ and inference results"""
+    path = os.path.join(config.paths.data_dir, "tiff_data", "ECOCLIMAP-SG-ML", "tif", f"ECOCLIMAP-SG-ML-v{config.versions.ecosgml}")
