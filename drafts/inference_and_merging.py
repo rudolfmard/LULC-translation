1de63e755df0b99a7d68a9dcbf00bd0159cccd46
Thomas Rieutord <thomas.rieutord@gmail.com>
2024-04-19 09:34:09 +0000

    Add argument parsing

--- drafts/inference_and_merging.py
+++ drafts/inference_and_merging.py
@@ -5,21 +5,48 @@
 Run inference and merge with ECOSG+ on large domains
 """
 
+import argparse
 import os
 
 from mmt import _repopath_ as mmt_repopath
 from mmt.datasets import landcovers
-from mmt.utils import domains
+from mmt.utils import domains, misc
 from mmt.inference import translators
 
 
 # Configs
 #------------
-checkpoint_path=os.path.join(mmt_repopath, "data", "saved_models", "mmt-weights-v1.0.ckpt")
-domainname = "montpellier_agglo"
-version = "v0.6"
-n_px_max = 600 # Maximum that could fit on the GPU?
-inference_dump_dir = os.path.join(mmt_repopath, "data", "outputs")
+default_output = os.path.join(mmt_repopath, "data", "outputs")
+
+parser = argparse.ArgumentParser(
+    prog="inference_and_merging",
+    description="Produce the ECOSG-ML map with the given weights on the given domain",
+    epilog="Example: python -i inference_and_merging.py --weights outofbox2 --npatches 200 --cpu",
+)
+parser.add_argument(
+    "--weights",
+    help="Weight file, experience ID or path to the checkpoint to use for inference",
+    default="mmt-weights-v1.0.ckpt",
+)
+parser.add_argument(
+    "--output",
+    help=f"Output directory is {default_output}",
+    default=default_output,
+)
+parser.add_argument("--domainname", help="Geographical domain name", default="montpellier_agglo")
+parser.add_argument("--patchsize", help="Size (#px of 10m) of the patches in the sampler", default=600, type=int) # Maximum that could fit on the GPU?
+parser.add_argument(
+    "--cpu", help="Perform inference on CPU", action="store_true", default=False
+)
+args = parser.parse_args()
+
+
+device = "cpu" if args.cpu else "cuda"
+checkpoint_path = misc.weights_to_checkpoint(args.weights)
+weights = misc.checkpoint_to_weight(checkpoint_path)
+domainname = args.domainname
+n_px_max = args.patchsize
+inference_dump_dir = args.output
 
 # Load translators
 #------------
@@ -31,8 +58,8 @@ qdomain = getattr(domains, domainname)
 #------------
 inference_tif_dir = translator.predict_from_large_domain(
     qdomain,
-    output_dir=os.path.join(inference_dump_dir, f"infres-{version}.{domainname}.[id]"),
-    tmp_dir=os.path.join(inference_dump_dir, f"infres-{version}.{domainname}.[id]"),
+    output_dir=os.path.join(inference_dump_dir, f"infres-{weights}.{domainname}.[id]"),
+    tmp_dir=os.path.join(inference_dump_dir, f"infres-{weights}.{domainname}.[id]"),
     n_px_max = n_px_max,
     n_max_files = 0,
 )
@@ -45,8 +72,8 @@ merger = translators.MapMerger(inference_tif_dir, merge_criterion = "qflag2_noda
 print(f"Merging criterion: {merger.merge_criterion.__doc__}")
 merging_dump_dir = merger.predict_from_large_domain(
     qdomain,
-    output_dir=os.path.join(inference_dump_dir, f"ECOSGML-{version}-[id]"),
-    tmp_dir=os.path.join(inference_dump_dir, f"merger-{version}.{domainname}.[id]"),
+    output_dir=os.path.join(inference_dump_dir, f"ECOSGML-{weights}-[id]"),
+    tmp_dir=os.path.join(inference_dump_dir, f"merger-{weights}.{domainname}.[id]"),
     n_px_max = 80, # Make sure that the patches are smaller than the size of a file
     # Thumb rule: < n_px_max*esawc.res/esgp.res
     n_max_files = 12, # Set to 0 to avoid clustering (only copy from tmp_dir to output_dir)
