f114bc3b8d4a8d8ba68a6bf34207658a5f36d7e7
Thomas Rieutord <thomas.rieutord@gmail.com>
2024-04-19 09:33:33 +0000

    Testing program for export methods

---
+++ tests/export_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Multiple land-cover/land-use Maps Translation (MMT)
+
+Program to test the exportation of data
+"""
+
+import os
+from mmt.datasets import landcovers
+from mmt.utils import domains
+
+qb = domains.dublin_city.to_tgbox()
+os.makedirs("tmp", exist_ok=True)
+
+for lc in [
+    landcovers.EcoclimapSGplus(),
+    landcovers.ESAWorldCover(),
+    landcovers.EcoclimapSG(),
+]:
+    x = lc[qb]
+    print(f"Loaded {x['mask'].shape} from {lc.__class__.__name__}")
+
+    for fmt in ["dir", "tif", "npy", "nc"]:
+        newfile = lc.export(x, os.path.join("tmp", f"test.{fmt}"))
+        print(f"   Written: {newfile}")
